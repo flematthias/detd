bca17c69de4b60ad6950aae69a5f2b5191b60ac2
swikenson <wikensonsimond@hotmail.com>
2019-03-06 14:05:28 +0100

    8488

--- footer.php
+++ footer.php
@@ -3,16 +3,7 @@
 
 
 <footer>
-    <div id="banner">
-        <h1>D&D</h1>
-        <p>Design et Développement Web</p>
-        <i class="fa fa-facebook-square"></i>
-        <i class="fa fa-twitter"></i>
-        <i class="fa fa-dribbble"></i>
-        <i class="fa fa-github-alt"></i>
-        <i class="fa fa-codepen"></i>
-        <i class="fa fa-linkedin"></i>
-    </div>
+
 
     <nav>
     <?php wp_nav_menu(array(
