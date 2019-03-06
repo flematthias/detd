2690a320b4cc93c7e11645cb266fda518685eceb
flematthias <37439367+flematthias@users.noreply.github.com>
2019-03-06 11:17:33 +0100

    zeeezfe

--- home.php
+++ home.php
@@ -1,18 +1,20 @@
 <?php get_header(); ?>
 <?php
   $args = array(
-		'numberposts' => 10,
+		'numberposts' => 4,
 );
 
 global $post;
 
-$custom_posts = get_posts($args);
+$custom_posts = get_posts($args);?>
 
+<ul class="mdc-image-list mdc-image-list--masonry my-masonry-image-list">
 
+<?php
 foreach($custom_posts as $post) :setup_postdata($post);
 				get_template_part('loops/loop');
 
 			endforeach;
 		?>
-
+</ul>
 <?php get_footer(); ?>
--- loops/loop.php
+++ loops/loop.php
@@ -1,11 +1,11 @@
-  <ul class="mdc-image-list mdc-image-list--masonry my-masonry-image-list">
+
   <li class="mdc-image-list__item">
-    <img class="mdc-image-list__image" src="<?php echo get_the_post_thumbnail_url();?>">
-    <div class="mdc-image-list__supporting">
-      <span class="mdc-image-list__label"><?php the_title(); ?></span>
-      <span class="mdc-image-list__label"><?php the_time('j F Y, '); ?></span>
-      <span class="mdc-image-list__label"><?php the_category(', '); ?></span>
+    <div>
+        <img class="mdc-image-list__image" src="<?php echo get_the_post_thumbnail_url();?>">
+        <div class="mdc-image-list__supporting">
+          <span class="mdc-image-list__label"><?php the_title(); ?></span>
+          <span class="mdc-image-list__label"><?php the_time('j F Y, '); ?></span>
+          <span class="mdc-image-list__label"><?php the_category(', '); ?></span>
+        </div>
     </div>
   </li>
-
-</ul>
