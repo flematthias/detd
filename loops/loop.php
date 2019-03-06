c1b428cd015882d3c8c31d6c37dc8428eb5389b5
flematthias <37439367+flematthias@users.noreply.github.com>
2019-03-06 13:35:33 +0100

    cccc

--- loops/loop.php
+++ loops/loop.php
@@ -1,11 +1,10 @@
-
   <li class="mdc-image-list__item">
-    <div>
+   
         <img class="mdc-image-list__image" src="<?php echo get_the_post_thumbnail_url();?>">
-        <div class="mdc-image-list__supporting">
+        <div class="mdc-image-list__supporting centerhome">
           <span class="mdc-image-list__label"><?php the_title(); ?></span>
           <span class="mdc-image-list__label"><?php the_time('j F Y, '); ?></span>
           <span class="mdc-image-list__label"><?php the_category(', '); ?></span>
         </div>
-    </div>
+ 
   </li>
