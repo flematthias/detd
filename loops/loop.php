eb9e98863b90e97587b64f834c36a04d1051260b
flematthias <37439367+flematthias@users.noreply.github.com>
2019-03-06 15:22:49 +0100

    sffdsfdf

--- loops/loop.php
+++ loops/loop.php
@@ -1,4 +1,4 @@
-  <li class="mdc-image-list__item classhome mb-5">
+  <li class="mdc-image-list__item classhome mx-3 mb-5 ">
    
         <img class="mdc-image-list__image" src="<?php echo get_the_post_thumbnail_url();?>">
         <div class="mdc-image-list__supporting ">
