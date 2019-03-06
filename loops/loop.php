839011fb3c8e344de4c2f62a3fd937145cb5da1f
flematthias <37439367+flematthias@users.noreply.github.com>
2019-03-06 16:44:42 +0100

    ccc

--- loops/loop.php
+++ loops/loop.php
@@ -1,6 +1,9 @@
   <li class="mdc-image-list__item classhome mx-3 mb-5 ">
-        <div class="img-comments" style="background-image:url('<?php echo get_stylesheet_directory_uri(); ?>/images/comment.png');" ><p class="number-comments"><?php comments_number('0', '1', '%') ?></p></div>
+        
         <img class="mdc-image-list__image" src="<?php echo get_the_post_thumbnail_url();?>">
+        <div class="img-comments" style="background-image:url('<?php echo get_stylesheet_directory_uri(); ?>/images/comment.png');" >
+      
+        <p class="number-comments"><?php comments_number('0', '1', '%') ?></p></div>
         <div class="mdc-image-list__supporting ">
           <h3 class=""><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
           <span class=""><?php the_time('j F Y '); ?></span>
