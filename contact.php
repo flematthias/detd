9d3c5927660a8aae50c105e585ec9a5d1fb228b0
flematthias <37439367+flematthias@users.noreply.github.com>
2019-03-07 13:51:43 +0100

    jb

--- contact.php
+++ contact.php
@@ -8,7 +8,6 @@
     <h1>CONTACT</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis adipisci magnam sunt natus amet sapiente. Necessitatibus, rerum molestias sequi quaerat aliquid repellendus at! Tempora molestiae, consectetur placeat nostrum voluptatum qui?</p>
 
-    [contact-form-7 id="35" title="Formulaire de contact"]
 <?php
 
 
