794af0d3d467f6209a7bb898df6105088714164e
LaurineGillet <laurinenet@gmail.com>
2019-03-07 12:14:02 +0100

    aside in progress

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,5 @@
-<aside>
-    <?php if ( is_active_sidebar( 'aside-widget-area' ) ) : ?>
+ <?php if ( is_active_sidebar( 'aside-widget-area' ) ) : ?>
         <div id="aside-widget-area" class="nwa-aside-widget widget-area" role="complementary">
             <?php dynamic_sidebar( 'aside-widget-area' ); ?>
         </div>
     <?php endif; ?>
-</aside>
