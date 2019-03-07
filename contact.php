c6372760a0c22ce4587a6a85af2a3c447cbf9d5c
swikenson <wikensonsimond@hotmail.com>
2019-03-07 15:52:45 +0100

    78539521852

--- contact.php
+++ contact.php
@@ -3,21 +3,19 @@
 * Template Name: contact
 */
 
- get_header(); ?>
-
-    <h1>CONTACT</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis adipisci magnam sunt natus amet sapiente. Necessitatibus, rerum molestias sequi quaerat aliquid repellendus at! Tempora molestiae, consectetur placeat nostrum voluptatum qui?</p>
 
 
-<div class="container">
 
-<?php
-if ( have_posts() ) : while ( have_posts() ) : the_post();
-the_content();
-endwhile; else:
-endif;?>
-</div>
+get_header(); ?>
+<?php the_post(); ?>
 
+<section class="single_article">
+    <article class="pt-3">
+        <h2 class="contact"><?php the_title(); ?></h2>
+    <?php
+    the_content();
 
+    ?>
+    </article>
+</section>
 <?php get_footer(); ?>
-
