c7b8db72f2eadef116971994aa14996ef871685a
LaurineGillet <laurinenet@gmail.com>
2019-03-07 14:18:00 +0100

    sidebar search&archive

--- search.php
+++ search.php
@@ -1,16 +1,15 @@
 <?php get_header(); ?>
+    <div class="sidebar_page_content">
+        <ul class="mdc-image-list mdc-image-list--masonry my-masonry-image-list container mb-5">
+           <?php
+           if ( have_posts() ) : while ( have_posts() ) : the_post();
+           get_template_part('loops/loop');
+           endwhile; else:?>
+            <h2 class="page-title">Aucun article ne correspond à votre recherche.</h2>
+          <?php endif;?>
+        </ul>
 
-
-<ul class="mdc-image-list mdc-image-list--masonry my-masonry-image-list container mb-5">
-   <?php
-   if ( have_posts() ) : while ( have_posts() ) : the_post();
-   get_template_part('loops/loop');
-   endwhile; else:?>
-    <h2 class="page-title">Aucun article ne correspond à votre recherche.</h2>
-<div class="align-center">
-    <img src="<?php echo get_template_directory_uri();?>/images/lunettes.jpg" alt="">
-</div>
-  <?php endif;?>
-</ul>
+        <?php get_template_part('sidebar'); ?>
+    </div>
 <div class="navigation mb-5"><?php bongo_wp_pagination(); ?></div>
 <?php get_footer(); ?>
