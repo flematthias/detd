cd114a296ad0244df97f77aec0824fa3ae5eb79c
swikenson <wikensonsimond@hotmail.com>
2019-03-07 12:14:03 +0100

    785269

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 
-mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
-
-$(document).ready(function(){
-
-
-})
-
+jQuery(function($) {
+    $(".menu-item-has-children").append("<i class='fa fa-plus'></i>");
+    $(".menu-item-has-children").mouseenter(function(){
+        $(".fa-plus").remove();
+        $(".fa-minus").remove();
+        $(this).append( "<i class='fa fa-minus'></i>");
+        $(".sub-menu").slideDown();
+    });
+    $(".menu-item-has-children").mouseleave(function(){
+        $(".fa-minus").remove();
+        $(this).append( "<i class='fa fa-plus'></i>");
+        $(".sub-menu").hide();
+    });
+});
