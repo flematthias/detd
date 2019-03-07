733a94cf0a2edd8bfb9e7d2fc06b813f8dd7f4f0
flematthias <37439367+flematthias@users.noreply.github.com>
2019-03-07 14:09:40 +0100

    h

--- search.php
+++ search.php
@@ -5,8 +5,12 @@
    <?php
    if ( have_posts() ) : while ( have_posts() ) : the_post();
    get_template_part('loops/loop');
-   endwhile; else:
-   endif;?>
+   endwhile; else:?>
+    <h2 class="page-title">Aucun article ne correspond à votre recherche.</h2>
+<div class="align-center">
+    <img src="<?php echo get_template_directory_uri();?>/images/lunettes.jpg" alt="">
+</div>
+  <?php endif;?>
 </ul>
 <div class="navigation mb-5"><?php bongo_wp_pagination(); ?></div>
 <?php get_footer(); ?>
