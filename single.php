26e11b7821d9b9a35ad8f34058bb5ccd2c308b57
LaurineGillet <laurinenet@gmail.com>
2019-03-07 10:42:29 +0100

    single

--- single.php
+++ single.php
@@ -9,9 +9,9 @@
         <div>
             <header>
                 <h2><?php the_title(); ?></h2>
-                <p><?php echo get_the_date(); ?><br>
-                    <?php the_category(','); ?>
-                </p>
+                <p><?php echo get_the_date(); ?><br></p>
+                <p><?php the_category(','); ?></p>
+
             </header>
             <p><?php the_content(); ?></p>
             <p>Publié par: <?php the_author(); ?></p>
@@ -21,11 +21,10 @@
     <?php comments_template(); ?>
     </article>
 
-    <div class="previous_article">
-        <?php previous_post_link(); ?>
+    <div class="prev_next_article">
+            <?php previous_post_link('%link'); ?>
+            <?php next_post_link('%link'); ?>
     </div>
-    <div class="next_article">
-        <?php next_post_link(); ?>
-    </div>
+
 </section>
 <?php get_footer(); ?>
